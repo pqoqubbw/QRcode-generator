45bb82d8df5c45c88932562a0adc5c4d9a42d2a6
pqoqubbw <ifffweknow@gmail.com>
2021-01-21 23:16:45 +0200

    add files

---
+++ script.js
@@ -0,0 +1,35 @@
+let qrcode = document.querySelector('img');
+let qrtext = document.querySelector('textarea');
+let qrbtn = document.querySelector('.generate');
+let qrDownload = document.querySelector('.download')
+
+qrbtn.addEventListener('click', generateQR);
+
+function generateQR(){
+    let size = '1000x1000';
+    let data = qrtext.value;
+    let baseURL = 'http://api.qrserver.com/v1/create-qr-code/';
+    
+    let url = `${baseURL}?data=${data}&size=${size}`;
+
+    qrcode.src = url;
+
+    qrDownload.style.display = 'block';
+}
+
+$('.download').on('click', function(){
+	$.ajax({
+		url: `${qrcode.src}`,
+		dataType: 'binary',
+		xhrFields: {
+			'responseType': 'blob'
+		},
+		success: function(data, status, xhr) {
+			var blob = new Blob([data], {type: xhr.getResponseHeader('Content-Type')});
+			var link = document.createElement('a');
+			link.href = window.URL.createObjectURL(blob);
+			link.download = 'QR-code.png';
+			link.click();
+		}
+	});
+});
