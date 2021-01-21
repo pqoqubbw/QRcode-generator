d810842c87e0f41f1d84f68944089b9ce2af4984
pqoqubbw <71014515+pqoqubbw@users.noreply.github.com>
2021-01-21 23:32:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ qrbtn.addEventListener('click', generateQR);
 function generateQR(){
     let size = '1000x1000';
     let data = qrtext.value;
-    let baseURL = 'http://api.qrserver.com/v1/create-qr-code/';
+    let baseURL = 'https://api.qrserver.com/v1/create-qr-code/';
     
     let url = `${baseURL}?data=${data}&size=${size}`;
 
